65484becdfdc49d4944af78aa4795ae9be27a319
msifatsadiq <msifatsadiq@gmail.com>
2025-06-26 16:37:07 +0600

    implementing the Trailer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeaturedSection from "../components/FeaturedSection"
 import HeroSection from "../components/HeroSection"
+import TrailerSection from "../components/TrailerSection"
 
 const Home = () => {
     return (
         <div>
             <HeroSection />
             <FeaturedSection />
+            <TrailerSection />
         </div>
     )
 }
